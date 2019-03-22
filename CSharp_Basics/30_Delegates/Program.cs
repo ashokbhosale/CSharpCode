942bc5ac1cc7b7eef34a6ef85acd7f8ab2c9f528
Bhosale, Ashok <Ashok.Bhosale@ge.com>
2019-03-22 17:08:53 +0530

    70-483 docs

--- CSharp_Basics/30_Delegates/Program.cs
+++ CSharp_Basics/30_Delegates/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+delegate int NumberChanger(int n);
 namespace _30_Delegates
 {
     class Program
     {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
         static void Main(string[] args)
         {
+            //create delegate instances
+            NumberChanger nc;
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
+
+            nc = nc1;
+            nc += nc2;
+
+            //calling multicast
+            nc(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
         }
     }
 }
